99ea9e64e730260bfeb51828d455daa9b1e01f54
Joshardals <joshuabamidele219@gmail.com>
2022-02-03 17:50:31 +0100

    initial commit

--- http-stream.js
+++ http-stream.js
@@ -1,9 +1,17 @@
 const http = require("http");
-const { readFileSync } = require("fs");
+const fs = require("fs");
 
 http
   .createServer((req, res) => {
-    const text = readFileSync("./content/bigFile.txt", "utf8");
-    res.end(text);
+    const fileStream = fs.createReadStream("./content/bigFile.txt", "utf8");
+    fileStream.on("open", () => {
+      fileStream.pipe(res);
+    });
+    fileStream.on("error", (err) => {
+      res.end(err);
+    });
+
+    // const text = fs.readFileSync("./content/bigFile.txt", "utf8");
+    // res.end(text);
   })
   .listen(3000);
